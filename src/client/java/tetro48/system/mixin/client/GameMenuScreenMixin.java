0f0b8b533daf26bf07b1b95b483b1bade9beced7
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-04-10 10:35:14 +0700

    Hunger no longer shakes on login

---
+++ src/client/java/tetro48/system/mixin/client/GameMenuScreenMixin.java
@@ -0,0 +1,16 @@
+package tetro48.system.mixin.client;
+
+import net.minecraft.client.gui.screen.GameMenuScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import tetro48.system.GranularHungerClient;
+
+@Mixin(GameMenuScreen.class)
+public abstract class GameMenuScreenMixin {
+    @Inject(method = "disconnect", at = @At("TAIL"))
+    private void resetClientExhaustion(CallbackInfo ci) {
+        GranularHungerClient.publicExhaustionValue = -1f;
+    }
+}
