c9199862e464f94e65b8d20ac42ff42dabf22790
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-04-10 10:51:02 +0700

    Added hunger pips item component for higher resolution food items

--- src/main/java/tetro48/system/GranularHunger.java
+++ src/main/java/tetro48/system/GranularHunger.java
@@ -1,8 +1,12 @@
 package tetro48.system;
 
+import com.mojang.serialization.Codec;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.minecraft.component.ComponentType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +15,11 @@ public class GranularHunger implements ModInitializer {
 	public static final String MOD_ID = "granular-hunger";
 
 	public static final Identifier EXHAUSTION_UPDATE_PACKET_ID = Identifier.of(MOD_ID, "exhaustion_update");
-
+	public static final ComponentType<Integer> HUNGER_PIP_COMPONENT = Registry.register(
+			Registries.DATA_COMPONENT_TYPE,
+			Identifier.of(GranularHunger.MOD_ID, "hunger_pips"),
+			ComponentType.<Integer>builder().codec(Codec.INT).build()
+	);
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
--- src/main/java/tetro48/system/mixin/PlayerEntityMixin.java
+++ src/main/java/tetro48/system/mixin/PlayerEntityMixin.java
@@ -1,9 +1,12 @@
 package tetro48.system.mixin;
 
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.world.World;
@@ -14,6 +17,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import tetro48.system.GranularHunger;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
@@ -26,9 +30,17 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 
     @Shadow public abstract void playSound(SoundEvent sound, float volume, float pitch);
 
+    @Shadow protected HungerManager hungerManager;
+
     @Inject(method = "tick", at = @At("HEAD"))
     private void tickCustom(CallbackInfo ci) {
         remainingUnableToConsumeTicks--;
+    }
+
+    @Inject(method = "eatFood", at = @At("HEAD"))
+    private void onEatFood(World world, ItemStack stack, FoodComponent foodComponent, CallbackInfoReturnable<ItemStack> cir) {
+        int hungerPips = stack.getOrDefault(GranularHunger.HUNGER_PIP_COMPONENT, 0);
+        hungerManager.add(hungerPips, foodComponent.saturation());
     }
     @Inject(method = "canConsume", at = @At("RETURN"), cancellable = true)
     private void modifyCanConsume(boolean ignoreHunger, CallbackInfoReturnable<Boolean> cir) {
