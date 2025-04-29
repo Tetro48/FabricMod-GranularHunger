e00191fbe99f60c5e343248aecf8a7a7fd984613
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-04-29 14:12:06 +0700

    fix wrong fat gain once again also bump patch version

--- src/main/java/tetro48/system/HungerManagerHelper.java
+++ src/main/java/tetro48/system/HungerManagerHelper.java
@@ -5,6 +5,6 @@ import net.minecraft.entity.player.HungerManager;
 
 public class HungerManagerHelper {
 	public static void eatCombined(HungerManager hungerManager, FoodComponent foodComponent, int hungerPips) {
-		hungerManager.addInternal(foodComponent.nutrition() * 3 + hungerPips, foodComponent.saturation());
+		hungerManager.addInternal(foodComponent.nutrition() * 3 + hungerPips, foodComponent.saturation() * 3);
 	}
 }
